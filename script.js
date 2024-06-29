a1a4d1966cbdacf9308a36e112cf80c320265151
BloodthirstyWolf <138318964+BloodthirstyWolf@users.noreply.github.com>
2024-06-29 07:22:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const heroImage = document.querySelector('.hero-image');
+    const getHelpButtons = document.querySelectorAll('.btn.get-help');
     const ovalShape = document.querySelector('.oval-shape');
-    const personWithDog = document.querySelector('.hero-image');
-    const getHelpButton = document.querySelector('.get-help');
 
     function updateOvalPosition() {
-        const personRect = personWithDog.getBoundingClientRect();
-        const buttonRect = getHelpButton.getBoundingClientRect();
+        const heroImageRect = heroImage.getBoundingClientRect();
+        const bottomGetHelpButton = getHelpButtons[getHelpButtons.length - 1].getBoundingClientRect();
 
-        const ovalTop = personRect.top - (window.innerHeight * 0.1);
-        const ovalBottom = buttonRect.bottom + (window.innerHeight * 0.1);
+        const ovalTop = heroImageRect.top - (window.innerHeight * 0.1);
+        const ovalBottom = bottomGetHelpButton.bottom + (window.innerHeight * 0.1);
 
-        ovalShape.style.top = ovalTop + 'px';
-        ovalShape.style.height = (ovalBottom - ovalTop) + 'px';
+        ovalShape.style.top = `${ovalTop}px`;
+        ovalShape.style.height = `${ovalBottom - ovalTop}px`;
     }
 
     window.addEventListener('resize', updateOvalPosition);
