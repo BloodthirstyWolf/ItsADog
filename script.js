561ab1ad06f40e2ca7ff94e0fcd5119ba98665bc
BloodthirstyWolf <138318964+BloodthirstyWolf@users.noreply.github.com>
2024-07-07 17:21:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function positionOvalShape() {
         const heroImageRect = heroImage.getBoundingClientRect();
-        const heroImageBottom = heroImageRect.bottom;
+        const heroImageTop = heroImageRect.top;
         const windowHeight = window.innerHeight;
-        const windowWidth = window.innerWidth;
 
         // Установка вертикальной позиции
-        const ovalTop = heroImageBottom - (windowHeight * 0.2); // Начать на 20vh выше нижней части hero-image
+        const ovalTop = heroImageTop - (windowHeight * 0.2); // Начать на 20vh выше верхней части hero-image
         ovalShape.style.top = `${ovalTop}px`;
-
-        // Установка горизонтальной позиции в центре экрана
-        const ovalLeft = (windowWidth / 2) - (ovalShape.offsetWidth / 2);
-        ovalShape.style.left = `${ovalLeft}px`;
-
-        // Установка высоты овала
-        const ovalHeight = windowHeight * 1.2; // Высота овала на 120vh
-        ovalShape.style.height = `${ovalHeight}px`;
     }
-
-    window.addEventListener('resize', positionOvalShape);
-    window.addEventListener('scroll', positionOvalShape);
 
     positionOvalShape(); // Вызываем функцию для первоначальной установки позиции
 });
