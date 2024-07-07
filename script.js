4131a16e27e0f62f83bf23c490fee41e7d96f3f0
BloodthirstyWolf <138318964+BloodthirstyWolf@users.noreply.github.com>
2024-07-07 15:36:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const heroImage = document.querySelector('.hero-image');
-    const getHelpButtons = document.querySelectorAll('.btn.get-help');
     const ovalShape = document.querySelector('.oval-shape');
 
     function updateOvalPosition() {
         const heroImageRect = heroImage.getBoundingClientRect();
-        const bottomGetHelpButton = getHelpButtons[getHelpButtons.length - 1].getBoundingClientRect();
+        const heroImageBottom = heroImageRect.bottom + window.scrollY; // Позиция нижней границы hero-image
 
-        const ovalTop = heroImageRect.top - (window.innerHeight * 0.1);
-        const ovalBottom = bottomGetHelpButton.bottom + (window.innerHeight * 0.1);
+        const ovalHeight = window.innerHeight * 0.2; // Высота овала (20vh)
+
+        const ovalTop = heroImageBottom - ovalHeight; // Позиция верхней границы овала
 
         ovalShape.style.top = `${ovalTop}px`;
-        ovalShape.style.height = `${ovalBottom - ovalTop}px`;
     }
 
     window.addEventListener('resize', updateOvalPosition);
