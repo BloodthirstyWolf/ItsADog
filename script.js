27d04bb33c98c882c40951f0aff8bd1153f3a69d
BloodthirstyWolf <138318964+BloodthirstyWolf@users.noreply.github.com>
2024-12-12 20:10:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-window.addEventListener('load', function() {
-    const headerHeight = document.querySelector('.hero h1').offsetHeight;
-    const headerMarginTop = parseInt(getComputedStyle(document.querySelector('.hero h1')).marginTop);
-    const headerMarginBottom = parseInt(getComputedStyle(document.querySelector('.hero h1')).marginBottom);
-    const descriptionHeight = document.querySelector('.right-column p.subtitle').offsetHeight;
-    const descriptionMarginTop = parseInt(getComputedStyle(document.querySelector('.right-column p.subtitle')).marginTop);
-    const descriptionMarginBottom = parseInt(getComputedStyle(document.querySelector('.right-column p.subtitle')).marginBottom);
-    
-    const totalHeight = headerHeight + headerMarginTop + headerMarginBottom + descriptionHeight + descriptionMarginTop + descriptionMarginBottom + 20; // 20px отступ
-    
-    document.querySelector('.arrow').style.top = totalHeight + 'px';
-});
+  const menuToggle = document.querySelector('.mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    menuToggle.addEventListener('click', function() {
+        menuToggle.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    })
