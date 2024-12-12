a43baafdbc8fdb66eb885bf87a405606928ed1c3
BloodthirstyWolf <138318964+BloodthirstyWolf@users.noreply.github.com>
2024-12-12 21:23:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@
             this.classList.toggle('open'); // Добавляем/убираем класс "open" для анимации стрелки и открытия меню
         });
     });
+
+menuToggle.addEventListener('click', function() {
+    if (menuToggle.classList.contains('active')) {
+        mobileMenu.classList.remove('active');
+    } else {
+        mobileMenu.classList.add('active');
+    }
+    menuToggle.classList.toggle('active');
+});
