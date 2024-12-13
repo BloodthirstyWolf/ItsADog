771b1ed4e0d644e93070d94ee5f69cb63ff9d10a
BloodthirstyWolf <138318964+BloodthirstyWolf@users.noreply.github.com>
2024-12-13 18:12:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,3 @@ menuToggle.addEventListener('click', function() {
     menuToggle.classList.toggle('active');  // Поворот гамбургера
     mobileMenu.classList.toggle('active');  // Показ/скрытие мобильного меню
 });
-
-// Выпадающее меню
-const dropdowns = document.querySelectorAll('.dropdown');
-
-dropdowns.forEach(dropdown => {
-    const link = dropdown.querySelector('a');
-    const dropdownContent = dropdown.querySelector('.dropdown-content');
-    const arrow = dropdown.querySelector('.dropdown-arrow');
-
-    // Обработчик клика по выпадающему меню
-    link.addEventListener('click', function(e) {
-        e.preventDefault(); // Останавливаем стандартное поведение ссылки
-        dropdown.classList.toggle('active'); // Открываем/закрываем меню
-        arrow.classList.toggle('rotate');  // Поворачиваем стрелку
-    });
-});
