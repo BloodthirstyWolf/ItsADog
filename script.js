7679197e7cb0ef87bd0eb9b74015300b7843307d
BloodthirstyWolf <138318964+BloodthirstyWolf@users.noreply.github.com>
2024-12-20 21:22:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ menuToggle.addEventListener('click', function() {
     menuToggle.classList.toggle('active');  // Поворот гамбургера
     mobileMenu.classList.toggle('active');  // Показ/скрытие мобильного меню
 });
+
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    const arrow = dropdown.querySelector('.dropdown-arrow');
+    const content = dropdown.querySelector('.dropdown-content');
+
+    dropdown.addEventListener('mouseenter', () => {
+        content.style.display = 'block';
+        arrow.style.transform = 'rotate(180deg)';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        content.style.display = 'none';
+        arrow.style.transform = 'rotate(0deg)';
+    });
+});
